47e2f238e6c0db967f11eb3a2c0a17e1849513d0
Viktor Aleksandrov <viqto0r@gmail.com>
2023-07-21 21:44:28 +0300

    layout and adaptive five section(Catalog)

--- src/js/script.js
+++ src/js/script.js
@@ -14,4 +14,59 @@ $(document).ready(function () {
       },
     ],
   })
+
+  const cardInfo = () => {
+    const itemsContents = document.querySelectorAll('.catalog-item__content')
+    const itemsLists = document.querySelectorAll('.catalog-item__list')
+
+    const bindEvents = (className) => {
+      const target = document.querySelectorAll(className)
+      target.forEach((link, idx) => {
+        link.addEventListener('click', (e) => {
+          e.preventDefault()
+          itemsContents[idx].classList.toggle('catalog-item__content_active')
+          itemsLists[idx].classList.toggle('catalog-item__list_active')
+        })
+      })
+    }
+
+    bindEvents('.catalog-item__link_about')
+    bindEvents('.catalog-item__link_back')
+  }
+
+  const tabs = () => {
+    const tabsContainer = document.querySelector('.catalog__tabs')
+    const tabs = tabsContainer.querySelectorAll('.catalog__tab')
+    const tabsContent = document.querySelectorAll('.catalog__content')
+
+    tabsContainer.addEventListener('click', (e) => {
+      if (
+        e.target.classList.contains('catalog__tab') ||
+        e.target.parentNode.classList.contains('catalog__tab')
+      ) {
+        const idx = +e.target.closest('[data-tab]').dataset.tab
+
+        toggleItems(tabs, idx, 'catalog__tab_active')
+        toggleItems(tabsContent, idx, 'catalog__content_active')
+      }
+    })
+
+    const hiddenAllItems = (items, className) => {
+      items.forEach((item) => {
+        item.classList.remove(className)
+      })
+    }
+
+    const showItem = (items, idx, className) => {
+      items[idx].classList.add(className)
+    }
+
+    const toggleItems = (items, idx, className) => {
+      hiddenAllItems(items, className)
+      showItem(items, idx, className)
+    }
+	}
+	
+  tabs()
+  cardInfo()
 })
