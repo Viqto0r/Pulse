ed50fab27a6b0d9b4979c02e3c635dc25acd47bd
Viktor Aleksandrov <viqto0r@gmail.com>
2023-07-26 00:19:39 +0300

    refactoring gitignore

--- src/send.php
+++ src/send.php
@@ -51,7 +51,6 @@ try {
     //Attachments
     //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
     //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = 'Here is the subject';
